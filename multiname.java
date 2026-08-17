0c1818e25181b588c20564ff589259cc35577dde
Srujan S Pattanashetti <srujanshiva291@gmail.com>
2026-08-17 15:34:10 +0530

    Create multiname.java

---
+++ multiname.java
@@ -0,0 +1,31 @@
+public class multiname1{
+    public stactic void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter name of person1:");
+        String person1=sc.nextLine();
+
+        System.out.print("Enter name of person2:");
+        String person2=sc.nextLine();
+
+        System.out.print("Enter name of person3:");
+        String person3=sc.nextLine();
+
+        System.out.print("Enter name of person4:");
+        String person4=sc.nextLine();
+
+        System.out.print("Enter name of person5:");
+        String person5=sc.nextLine();
+
+        System.out.println("\nNames entered are:");
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+        System.out.println(person4);
+        System.out.println(person5);
+        
+
+
+
+    }
+}
