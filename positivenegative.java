0a12aa211ae7943a3c814e218a73681134acf222
Srujan S Pattanashetti <srujanshiva291@gmail.com>
2026-08-23 19:24:09 +0530

    Create positivenegative.java

---
+++ positivenegative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PositiveNegative {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number.");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println(number + " is zero.");
+        }
+
+        input.close();
+    }
+}
