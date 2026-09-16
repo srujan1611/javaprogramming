6543ea8360a4a37e17ee8028fcc73b12d52d0f4a
Srujan S Pattanashetti <srujanshiva291@gmail.com>
2026-09-16 23:18:22 +0530

    Create operatordetective.java

---
+++ operatordetective.java
@@ -0,0 +1,30 @@
+public class operatordetective
+{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // Addition
+        int sum = a + b;
+        System.out.println("Addition: " + sum);
+
+        // Subtraction
+        int difference = a - b;
+        System.out.println("Subtraction: " + difference);
+
+        // Multiplication
+        int product = a * b;
+        System.out.println("Multiplication: " + product);
+
+        // Division
+        int quotient = a / b;
+        System.out.println("Division: " + quotient);
+
+        // Modulus
+        int remainder = a % b;
+        System.out.println("Modulus: " + remainder);
+    }
+}
+
+
+
